15fbd3bf81814956297ee62caa360dfa20af8b36
Abhishek singh <147707693+DISCIPLINE022123@users.noreply.github.com>
2026-02-03 13:56:12 +0530

    Update AppContext.jsxapp to context

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,35 +2,35 @@ import { createContext, useState } from "react";
 
 export const AppContent = createContext();
 
-export const AppContextProvider = (props) => {
-    const backUrl = import.meta.env.VITE_BACK;
+export const AppContextProvider = ({ children }) => {
+  const backUrl = import.meta.env.VITE_BACKEND_URL;
 
-    const [isLoggedin, setLoggedIn] = useState(false);
-    const [userData, setUserData] = useState(null);
-    const [product , setProduct] = useState('');
-    const [sentEmail,setSentEmail ] = useState(false);
-    const [verify,setVerify]= useState(false);
-const [userEmail, setuserEmail]=useState('');
-    const value = {
-        backUrl,
-        isLoggedin,
-        setLoggedIn,
-        userData,
-        setUserData,
-        sentEmail,
-        setSentEmail,
-        verify,
-        setVerify,
-        userEmail, setuserEmail,
-        product , setProduct
-    
-         
-        // setter for storing email
-    };
+  const [isLoggedin, setLoggedIn] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const [product, setProduct] = useState("");
+  const [sentEmail, setSentEmail] = useState(false);
+  const [verify, setVerify] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
 
-    return (
-        <AppContent.Provider value={value}>
-            {props.children}
-        </AppContent.Provider>
-    );
+  const value = {
+    backUrl,
+    isLoggedin,
+    setLoggedIn,
+    userData,
+    setUserData,
+    product,
+    setProduct,
+    sentEmail,
+    setSentEmail,
+    verify,
+    setVerify,
+    userEmail,
+    setUserEmail,
+  };
+
+  return (
+    <AppContent.Provider value={value}>
+      {children}
+    </AppContent.Provider>
+  );
 };
